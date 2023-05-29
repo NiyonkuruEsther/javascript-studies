e4817076bacdb39ff745e2d73bc27ef09c536b79
estherniyo <estherniyonkuru123@gmail.com>
2023-05-29 10:09:27 +0200

    started the closures

---
+++ AdvancedFunctions/closures.js
@@ -0,0 +1,13 @@
+// Closures are a feature in JavaScript that allow an inner function to access and retain variables from its outer function, even after the outer function has finished executing.
+
+function outerFunction(outerParam) {
+  return function innerFunction(innerParam) {
+    console.log(outerParam + innerParam);
+  };
+}
+
+// To pass the outer Function you just pass as usual
+// To pass to the inner function you call the assigment to that inner function
+
+var closure = outerFunction(5); // Pass outer function
+closure(10); // Pass inner function
