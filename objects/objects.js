e206f88080a38e02f62842afc0ca80fbeadd4027
estherniyo <estherniyonkuru123@gmail.com>
2023-06-06 17:30:17 +0200

    finished the for-of

--- objects/objects.js
+++ objects/objects.js
@@ -2,4 +2,9 @@ const obj = { a: 1, b: 2, c: 3 };
 
 let a = "a";
 
-console.log(obj[a]);
+for (const elt in obj) {
+  //   console.log(elt); logs entries
+  console.log(obj[elt]); //logs values
+}
+
+// console.log(obj[a]);
