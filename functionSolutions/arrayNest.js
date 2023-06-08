305d5e44f997069cff398134effb002417006858
estherniyo <estherniyonkuru123@gmail.com>
2023-06-08 19:30:35 +0200

    add the concurrency part

--- functionSolutions/arrayNest.js
+++ functionSolutions/arrayNest.js
@@ -1,19 +1,20 @@
-function depth(arr){
-    let maxD = 0;
-    function arrayDepth(array, depth){
-        if(Array.isArray(array)){
-             depth++
-        if (depth > maxD) {
+function depth(arr) {
+  let maxD = 0;
+  function arrayDepth(array, depth) {
+    if (Array.isArray(array)) {
+      depth++;
+      if (depth > maxD) {
         maxD = depth;
       }
-       array.forEach(function(element) {
+      array.forEach(function (element) {
         arrayDepth(element, depth);
       });
-        }
-       
     }
-    arrayDepth(arr, 0);
-    return maxD;
+  }
+  arrayDepth(arr, 0);
+  return maxD;
 }
 
-console.log(depth([2,[3, 4[5, 7], [1,3]]]))
+console.log(
+  depth([1, 2, 3, [2, 3], [2, (4),[(5, 7)], [3, 4, [2, 5, 6, [7, 7]]]]])
+);
